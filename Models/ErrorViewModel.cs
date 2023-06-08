334c8ff0a187d05b947776c8d537caaec920d097
KasperLinux <kushaldas12364@gmail.com>
2023-06-08 10:39:25 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mbbs2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

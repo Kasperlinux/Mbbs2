3dd97b3637a42e4e0c850b13371a682deca7f53d
KasperLinux <kushaldas12364@gmail.com>
2023-06-19 13:04:49 +0530

    default

---
+++ Controllers/AppRolesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mbbs2.Controllers
+{
+    public class AppRolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AppRolesController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        //List all the Roles created by Users
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IdentityRole model)
+        {
+            //avoid duplicate role
+            if (!roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
